fe6446a5196f59078ead2948375c065a14ecd12c
jazmin <gracielajazmin_chavez@ucol.mx>
2019-11-10 10:18:16 -0600

    cambios agregados

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,71 +3,112 @@ import Head from 'next/head'
 import Registro from '../pages/registro'
 
 const Home = () => (
-  <div>
+
+  <div className ="inicio" id="inicio">
     <Head>
       <title>Home</title>
       <link rel='icon' href='/favicon.ico' />
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"></link>
     </Head>
-    
-    <div className="container-all col-md-12 m-5">
-      <div className ="container form m-5">
+    <div className="container-all col-md-12" id="form">
+    <div className = "row">
+      <div  className ="container-form shadow-lg p-3 rounded text-white ">
 
-        <img src="/login-png-1.png" className="img-thumbnail m-5" alt="Responsive image"></img>
+        <img src="/man-user.png" className="imagen" alt="Responsive image"></img>
         
-        <h1 className="">Iniciar Sesion</h1>
-          <form>
+        <h1 className="text-center">Login</h1>
+          <form className = "">
             <div className="form-group">
-              <label forhtml="exampleInputEmail1">Email</label>
-              <input className="form-control" type="email" id="email" placeholder="Enter email"></input>
+              <label forhtml="">Username</label>
+              <input className="formcontrol" autoComplete="off" type="text" id="email" placeholder="Username"></input>
             </div>
 
-            <div class="form-group">
-              <label forhtml ="exampleInputEmail1"> Contraseña</label>
-              <input className="form-control" type="password" id="password" placeholder="Enter contraseña"></input>              
+            <div className="form-group">
+              <label forhtml =""> Password</label>
+              <input className="formcontrol" type="password" id="password" placeholder="**********"></input>              
             </div>            
           </form>
 
-          <input type ="submit" className="btn btn-primary mb-5" value = "iniciar"></input>
+          <input type ="submit" className="btn btn-primary mb-5 d-flex justify-content-center" value = "SIGN IN"></input>
           
           <span className = "text-footer">¿Aún no te has registrado?
           <a href= "/registro">Registrate</a></span>
       </div>
+      </div>
+      <div  className ="piepagina">
+        <p>
+          Universidad de colima
+        </p>
+      </div>
     </div>
 
 
-    <style jsx> {`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
+    <style global jsx> {`        
+    @import url('https://fonts.googleapis.com/css?family=Libre+Franklin&display=swap');
       .row {
         max-width: 880px;
         margin: 80px auto 40px;
         display: flex;
         flex-direction: row;
         justify-content: space-around;
-        
+        font-family: 'Libre Franklin', sans-serif;
       }
-      .img-thumbnail{
+      .imagen{
         width:100px;
-        heigth1:100px;
+        heigth:100px;
+        margin-left: 104px;
+        padding: .25rem;
+        /* background-color: #fff; */
+        /* border: 1px solid #dee2e6; */
+        border-radius: .25rem;
+        max-width: 100%;
+        height: auto;
 
       }
-      
       .name{
         width: 411px;
+      }
+      .inicio{
+        background-image :url(/circuit-sensor-background-1.jpg);
+        backgroundColor:black;
+        margin-top: -84px;
+
+      }
+      .btn{
+        width: 239px;
+        height: 42px;
+        margin-left: 40px;
+        border-radius:4.25rem;
+        background-color:#38a1a4;
+      }
+      .container-form{
+        margin-top: 100px;
+        margin-bottom: 89px;
+        width: 358px;
+        background-Color: #607d8b85;
+
+      }
+      input{
+        border-radius:4.25rem;
+      }
+      .formcontrol{
+        display: block;
+        width: 100%;
+        height: calc(1.5em + .75rem + 2px);
+        padding: .375rem .75rem;
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.5;
+        color: #f5f3f3;
+        background-color: #3a383999;
+        background-clip: padding-box;
+        border: 1px solid #3a383999;
+        border-radius: 6.25rem;
+        -webkit-transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+        transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+      }
+      piepagina{
+        background-Color:black;
       }
 
     `}</style>
--- frontend/pages/registro.js
+++ frontend/pages/registro.js
@@ -2,70 +2,130 @@ import React from 'react'
 import Head from 'next/head'
 import Nav from '../components/nav'
 
-
 const Registro = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel='icon' href='/favicon.ico' />
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"></link>
-    </Head>
-    <Nav/>
-    <div className="container">
-    <div className ="row col-md-12">
-      <div className ="container form col-12 col-md-12 col-ms-12">
-      <img src="/login-png-1.png" className="img-thumbnail" alt="Responsive image"></img>
-      <h1>Registro</h1>
-      <form>
-        <label forhtml="">Email</label>
-        <input type = "text"></input>
-        <label forhtml =""> Contraseña</label>
-        <input type = "password"></input>
-        <input type ="submit" className="btn btn-primary" value = "iniciar"></input>
-        </form>
-        <span className = "text-footer">¿Aún no te has registrado?
-        <a href= "/index">Regresar</a></span>
-      </div>
-      </div>
-    </div>
+  <div className ="inicio" id="inicio">
+  <Head>
+    <title>Home</title>
+    <link rel='icon' href='/favicon.ico' />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"></link>
+  </Head>
+  
+  <Header authenticated={false} pathname={url.pathname} />
+  {error && <Error message={error} />}
+  <div className="container-all col-md-12" id="form">
+  <div className = "row">
+    <div  className ="container-form shadow-lg p-3 rounded text-white ">
 
-
-    <style jsx> {`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-        
-      }
-      .img-thumbnail{
-        width:100px;
-        heigth1:100px;
-
-      }
+      <img src="/man-user.png" className="imagen" alt="Responsive image"></img>
       
-      .name{
-        width: 411px;
-      }
+      <h1 className="text-center">Register </h1>
+        <form onSubmit={this.handleSubmit} className = "">
+          <div className="form-group">
+            <label forhtml="">Username</label>
+            <input className="formcontrol" autoComplete="off" type="text" id="email" placeholder="Username"></input>
+          </div>
 
-    `}</style>
+          <div className="form-group">
+            <label forhtml =""> Email</label>
+            <input className="formcontrol" type="email" placeholder="xxxxxx@ejemplo.com"></input>              
+          </div>  
+
+          <div className="form-group">
+            <label forhtml =""> Password</label>
+            <input className="formcontrol" type="password"  placeholder="**********"></input>              
+          </div>  
+
+          <div className="form-group">
+            <label forhtml ="">Key</label>
+            <input className="formcontrol" type="number" placeholder="45632"></input>              
+          </div>  
+       
+        </form>
+
+        <input type ="submit" className="btn btn-primary mb-5 d-flex justify-content-center" value = "SEND"></input>
+        
+        <span className = "text-footer">
+        <a href= "/login">Login In</a></span>
+    </div>
+    </div>
+    <div  className ="piepagina">
+      <p>
+        Universidad de colima
+      </p>
+    </div>
   </div>
+
+
+  <style global jsx> {`        
+  @import url('https://fonts.googleapis.com/css?family=Libre+Franklin&display=swap');
+    .row {
+      max-width: 880px;
+      margin: 80px auto 40px;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+      font-family: 'Libre Franklin', sans-serif;
+    }
+    .imagen{
+      width:100px;
+      heigth:100px;
+      margin-left: 104px;
+      padding: .25rem;
+      /* background-color: #fff; */
+      /* border: 1px solid #dee2e6; */
+      border-radius: .25rem;
+      max-width: 100%;
+      height: auto;
+
+    }
+    .name{
+      width: 411px;
+    }
+    .inicio{
+      background-image :url(/circuit-sensor-background-1.jpg);
+      backgroundColor:black;
+      margin-top: -84px;
+
+    }
+    .btn{
+      width: 239px;
+      height: 42px;
+      margin-left: 40px;
+      border-radius:4.25rem;
+      background-color:#38a1a4;
+    }
+    .container-form{
+      margin-top: 21px;
+      margin-bottom: 89px;
+      width: 358px;
+      background-Color: #607d8b85;
+
+    }
+    input{
+      border-radius:4.25rem;
+    }
+    .formcontrol{
+      display: block;
+      width: 100%;
+      height: calc(1.5em + .75rem + 2px);
+      padding: .375rem .75rem;
+      font-size: 1rem;
+      font-weight: 400;
+      line-height: 1.5;
+      color: #f5f3f3;
+      background-color: #3a383999;
+      background-clip: padding-box;
+      border: 1px solid #3a383999;
+      border-radius: 6.25rem;
+      -webkit-transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+      transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    }
+    piepagina{
+      background-Color:black;
+    }
+
+  `}</style>
+</div>
 )
 
 export default Registro
